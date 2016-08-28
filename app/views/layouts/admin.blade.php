c414b26202ffb43f6546efc4b24c4ee75fee6322
Reyn Tancio <1mr3yn@gmail.com>
2016-08-28 13:33:26 +0800

    typo fixed

--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -38,9 +38,9 @@
     <!-- Logo -->
     <a href="" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b class='text-yellow'>G</b><b class='text-primary'>lend</b></span>
+      <span class="logo-mini"><b class='text-blue'>G</b><b class='text-primary'>lend</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b class='text-yellow'>G</b><b class='text-primary'>lend</b></span>
+      <span class="logo-lg"><b class='text-blue'>G</b><b class='text-primary'>lend</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
